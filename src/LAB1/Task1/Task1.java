5cd187d58f5a6f5c7befcd374e205a8edb86d810
olinicenkovladislav <olinicenkovladislav149@example.com>
2025-03-20 21:21:25 +0200

    Виправлено конфлікти

--- src/LAB1/Task1/Task1.java
+++ src/LAB1/Task1/Task1.java
@@ -6,7 +6,11 @@ class UserAge{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введіть ваш вік: ");
+<<<<<<< HEAD
         int age = scanner.nextInt(); // Зчитуємо введене число       
+=======
+        int age = scanner.nextInt();
+>>>>>>> 50a8d21 (Додав нÐFWO COMMIt¸)
         System.out.println("Ваш вік: " + age + " років.");
         scanner.close();
     }
---
+++ src/LAB3/task2/task2.java
@@ -0,0 +1,27 @@
+package LAB3.task2;
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        int secretNumber = 7;
+        int userGuess;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.print("Вгадайте число від 1 до 10: ");
+            userGuess = scanner.nextInt();
+
+            if (userGuess < secretNumber) {
+                System.out.println("Загадане число більше.");
+            } else if (userGuess > secretNumber) {
+                System.out.println("Загадане число менше.");
+            } else {
+                System.out.println("Вітаємо! Ви вгадали число.");
+            }
+        } while (userGuess != secretNumber);
+
+        scanner.close();
+    }
+}
+
