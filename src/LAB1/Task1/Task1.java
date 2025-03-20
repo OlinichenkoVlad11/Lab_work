e1294bc02d5e72a8e7d7deda26614af5dbd73987
olinicenkovladislav <olinicenkovladislav149@example.com>
2025-03-20 21:21:25 +0200

    Виправлено конфлікти
    # Conflicts:
    #	src/LAB1/Task1/Task1.java

--- src/LAB1/Task1/Task1.java
+++ src/LAB1/Task1/Task1.java
@@ -6,7 +6,7 @@ class UserAge{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введіть ваш вік: ");
-        int age = scanner.nextInt();
+        int age = scanner.nextInt(); // Зчитуємо введене число       
         System.out.println("Ваш вік: " + age + " років.");
         scanner.close();
     }
