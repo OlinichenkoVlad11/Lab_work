50a8d21ce277c988922dced67d1e7670e57fed88
olinicenkovladislav <olinicenkovladislav149@example.com>
2025-03-20 21:16:24 +0200

    Додав нÐFWO COMMIt¸

---
+++ src/LAB3/task1/task1.java
@@ -0,0 +1,10 @@
+package LAB3.task1;
+
+class Main {
+    public static void main(String[] args) {
+        for (int i = 2; i <= 20; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
+
